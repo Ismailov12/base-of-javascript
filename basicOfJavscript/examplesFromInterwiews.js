dddf1e3d6329b516328643a0849c8e7452889c87
Orif <gofurov@mail.ru>
2020-12-25 10:02:20 +0500

    срфтув

---
+++ basicOfJavscript/examplesFromInterwiews.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// 1) XXX = 5
+// let x = 5;
+// alert(++x);
+
+// 2) 
+// XXX = NaN если мы выполяняем матем операции
+//  с массивами или строками то любое значение 
+//  преврашается в строку даже 
+//  если оно NUMBER || BOOLEAN;
+// alert([] + false - null + true);
+
+// 3)WELL DONE
+// let y = 1;
+// let x = y = 2;
+// alert(x);
+
+// 4) XXX = 12;
+// alert([] - 1 + 2);
+
+// 5) XXX = 1;
+// alert("1"[0]);
+
+// 6) XXX = NULL;
+// alert(2 && 1 && null && 0 && undefined);
+
+// 7) 50/50
+// let a = 1, b = 0;
+// alert(!!(a && b));
+// alert(a && b);
+
+// 8)WELL DONE
+// console.log(null || 2 & 3 || 4);
+
+// 9) status 50/50 different boxes
+// let a = [1,2,3], b = [1,2,3];
+// alert(a == b);
+// 10) WELL DONE
+alert( +"Inifinity" );
+
+// 11) status: FAILED
+// alert("Ёжик" > "яблоко");
+
+// 12)status: PASSED
+// alert(0 || "" || 2 || undefined || true || false); 
