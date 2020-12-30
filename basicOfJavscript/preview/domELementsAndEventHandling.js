1ac798bbaf782a19d6b2b39f71e62047afceaf9c
Orif <gofurov@mail.ru>
2020-12-30 11:44:25 +0500

    added new files

---
+++ basicOfJavscript/preview/domELementsAndEventHandling.js
@@ -0,0 +1,70 @@
+                /*** EVENT HANDLING ***/
+
+// const btn = document.querySelectorAll('button'),
+//     overlay = document.querySelector('.overlay'),
+//     href = document.querySelector("a"),
+//     incr = document.querySelector(".incr"),
+//     decr = document.querySelector(".decr");
+
+// let zero = document.querySelector(".zero");
+
+
+// let i = 0;
+// // btn.addEventListener('mouseenter', () => {
+// //     console.log("Hello");
+// // });
+
+
+// const delElem = (e) => {
+//     console.log(e.target);
+//     console.log(e.type);
+// };
+
+
+// function add(e) {
+//     e.target.value = i++;
+// }
+
+ 
+
+// incr.addEventListener("click", );
+
+// decr.addEventListener('click', );
+
+ 
+// btn.addEventListener('click', delElem);
+// overlay.addEventListener('click', delElem);
+// href.addEventListener('click', (e) => {
+
+//     console.log(e.target);
+// });
+
+
+// let matematikaXimiya = "11 topics";
+
+// ozbek tilini soxada qollash = 9topics
+
+// ingliz tili = 5topics
+
+// c++ = 10topics;
+ 
+
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+console.log(document.querySelector("#btn").parentNode.parentNode);
+
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
