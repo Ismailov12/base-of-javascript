56b83f979e65541d88e7a907542e351f42855097
Orif <gofurov@mail.ru>
2020-12-31 22:58:39 +0500

    added new Filter

--- basicOfJavscript/preview/domELementsAndEventHandling.js
+++ basicOfJavscript/preview/domELementsAndEventHandling.js
@@ -40,7 +40,7 @@
 // });
 
 
-// let matematikaXimiya = "11 topics";
+// let matematikaXimiyaFizika = "11 topics";
 
 // ozbek tilini soxada qollash = 9topics
 
@@ -56,15 +56,50 @@
 // console.log(document.body.firstChild);
 // console.log(document.body.lastChild);
 
-console.log(document.querySelector("#btn").parentNode.parentNode);
+// console.log(document.querySelector("#btn").parentNode.parentNode);
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
+// for (const node of document.body.childNodes) {
+//     const nodeName = node.nodeName;
+//     if(nodeName == 'BUTTON' || nodeName == "DIV" || nodeName == '#text' || nodeName == '#comment') {
+//         continue;
+//     }
+
+//     console.log(node);
+// }
+
+// console.log(document.body.childNodes);
+
+
+
+            /*** MOBILE EVENTSHANDLING ***/ 
+
+// TOUCHSTART - HANDLING WILL GET STARTED WHEN WE TOUCH THE SCREEN
+// TOUCHMOVE - HANDLING WILL GET STARTED WHEN WE MOVING THE SCREEN
+// TOUCHEND - HANDLING WILL GET STARTED WHEN WE REMOVE THE ARM FROM SCREEN
+// TOUCHENTER - 
+// TOUCHLEAVE -
+// TOUCHCANCEL - 
+
+window.addEventListener("DOMContentLoaded", (e) => {
+    e.preventDefault();
+
+    const btn = document.querySelector("#btn");
+
+    btn.addEventListener("touchstart", (e) => {
+        e.preventDefault();
+        console.log("HELLO");
+    });
+
+    btn.addEventListener("touchmove", (event) => {
+        event.preventDefault();
+        console.log("MOVE");
+    });
+
+    btn.addEventListener("touchend", (e) => {
+        e.preventDefault();
+
+        console.log("ENDED");
+    })
+});
+
--- basicOfJavscript/project/js/script.js
+++ basicOfJavscript/project/js/script.js
@@ -14,39 +14,146 @@
 
 'use strict';
 
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     event.preventDefault();
 
-let adver = document.querySelectorAll(".promo__adv img"),
-    bgImage = document.querySelector(".promo__bg"),
-    genre = bgImage.querySelector(".promo__genre"),
-      nameOfFilms = document.querySelectorAll(".promo__interactive-item");
-// 1)      
-// for(let i = 0; i < adver.length; i++) {
-//     adver[i].remove();
-// }
+//     const adv = document.querySelectorAll(".promo__adv img"),
+//         genre = document.querySelector(".promo__genre"),
+//         img = document.querySelector(".promo__bg"),
+//         films = document.querySelector(".promo__interactive-list");
 
-adver.forEach(item => {
-    item.remove();
-});
-// 2)
-genre.textContent = "Драма";
+//     adv.forEach((item) => {
+//         item.remove();
+//     });
 
-// 3)
-bgImage.style.cssText = "background: url(img/bg.jpg)  no-repeat; background-size: cover; ";
+//     genre.textContent = "Драма";
 
-// 4)
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-movieDB.movies.sort();
-nameOfFilms.forEach((item, i) => {
-    item.innerHTML = `
-    <li class="promo__interactive-item">${i + 1} ${movieDB.movies[i]}
-        <div class="delete"></div>
-    </li>`;
+//     img.style.backgroundImage = "url(img/bg.jpg)";
+
+//     const movieDB = {
+//         movies: [
+//             "Логан",
+//             "Лига справедливости",
+//             "Ла-ла лэнд",
+//             "Одержимость",
+//             "Скотт Пилигрим против..."
+//         ]
+//     };
+
+//     movieDB.movies.sort();
+
+//     films.innerHTML = "";
+
+//     movieDB.movies.forEach((film, i) => {
+//         films.innerHTML += `
+//         <li class="promo__interactive-item">${i + 1} ${film}
+//             <div class="delete"></div>
+//          </li>
+//         `;
+//     });
+// });
+
+/* Задания на урок:
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    event.preventDefault();
+
+    const adv = document.querySelectorAll(".promo__adv img"),
+        genre = document.querySelector(".promo__genre"),
+        img = document.querySelector(".promo__bg"),
+        filmsList = document.querySelector(".promo__interactive-list"),
+        addForm = document.querySelector(".add"),
+        input = document.querySelector(".adding__input"),
+        checkbox = document.querySelector("[type='checkbox']");
+
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+
+    addForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        let newFilm = input.value;
+        const checkBox = checkbox.checked;
+
+        if (newFilm) {
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+            if (checkBox) {
+                console.log(`The ${newFilm} added to favorites`);
+            }
+
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+            crtMovieList(movieDB.movies, filmsList);
+        }
+
+
+
+        event.target.reset();
+    });
+
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+    const crtMovieList = (film, parent) => {
+        parent.innerHTML = "";
+        sortArr(film);
+        film.forEach((film, i) => {
+            parent.innerHTML += `
+            <li class="promo__interactive-item">${i + 1} ${film}
+                <div class="delete"></div>
+             </li>
+            `;
+        });
+
+        document.querySelectorAll(".delete").forEach((btn, i) => {
+            btn.addEventListener("click", () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                crtMovieList(movieDB.movies, filmsList);
+            });
+        });
+
+
+    };
+
+    const delElem = (item) => {
+        item.forEach((data) => {
+            data.remove();
+        });
+    };
+
+    const makeChanges = (text, bg) => {
+        text.textContent = "Драма";
+        bg.style.backgroundImage = "url(img/bg.jpg)";
+    };
+
+
+    makeChanges(genre, img);
+    delElem(adv);
+    crtMovieList(movieDB.movies, filmsList);
 });
